871754fdf2812f2c5a0ce21ad0a25e2d7f6437de
Neil Walkinshaw <ndwalkinshaw@gmail.com>
2021-11-19 20:41:08 +0000

    Updated javadoc for framework.operators.Operator

--- src/java/main/framework/operators/Operator.java
+++ src/java/main/framework/operators/Operator.java
@@ -3,7 +3,14 @@ package framework.operators;
 public abstract class Operator<T> {
 
     /**
-     * Apply the operation. 
+     * Applies the operator and returns the result. 
+     * 
+     * Depending on the operator, there can be an arbitrary number of parameters.
+     * 
+     * The assumption is that the return type will be the same as the parameter types.
+     * 
+     * I.e. An operator for BinomialOpinions will take BinomialOpinion objects as parameters
+     * and will return a BinomialOpinion as output.
      * 
      * @param opinions
      */
